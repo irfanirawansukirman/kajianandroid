9fec6debfc4fd98c1e510a48c2cc2a62687eb7d9
Irfan Irawan Sukirman <irfan.irawan@gits.co.id>
2017-08-27 06:57:53 +0700

    Fix: create room dialog Fix: create room dialog animation

--- app/src/main/java/com/iriras/aplikasichatfirebase/mvp/roomlist/RoomListActivity.kt
+++ app/src/main/java/com/iriras/aplikasichatfirebase/mvp/roomlist/RoomListActivity.kt
@@ -1,5 +1,9 @@
 package com.iriras.aplikasichatfirebase.mvp.roomlist
 
+import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import com.iriras.aplikasichatfirebase.R
 import com.iriras.aplikasichatfirebase.base.BaseActivity
@@ -9,5 +13,17 @@ class RoomListActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.room_list_activity)
+    }
+
+    @SuppressLint("InflateParams")
+    private fun dummyDialog(){
+        val alertDialog = AlertDialog.Builder(this)
+        val inflater = this.layoutInflater
+        val view = inflater.inflate(R.layout.room_dialog, null)
+        alertDialog.setView(view)
+        val viewDialog = alertDialog.create()
+        viewDialog.window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        viewDialog.window.attributes.windowAnimations = R.style.MyDialogAnimation
+        viewDialog.show()
     }
 }
--- app/src/main/java/com/iriras/aplikasichatfirebase/util/helper/FxHelper.kt
+++ app/src/main/java/com/iriras/aplikasichatfirebase/util/helper/FxHelper.kt
@@ -6,10 +6,10 @@ import android.content.Context
  * Created by irfan on 8/24/17.
  */
 
-fun Int.pxToDp(context: Context, source: Int): Int {
-    return (source / context.resources.displayMetrics.density).toInt()
+fun Float.pxToDp(context: Context, source: Float): Float {
+    return source / context.resources.displayMetrics.density
 }
 
-fun Int.dpToPx(context: Context, source: Int): Int {
-    return (source * context.resources.displayMetrics.density).toInt()
+fun Float.dpToPx(context: Context, source: Float): Float {
+    return source * context.resources.displayMetrics.density
 }
